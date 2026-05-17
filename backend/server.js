ba3aa9465c182298e11429bcffaf861954e07c83
Shivam Rai <rai268359@gmail.com>
2026-05-17 23:29:51 +0530

    fixed deployment issue

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,34 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
 
+// ✅ CORS FIX FOR ALL DEVICES
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+
+// ✅ JSON PARSER
 app.use(express.json());
 
 
-// ROUTES
+// ✅ ROUTES
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
 app.use("/api/ai", require("./routes/aiRoutes"));
 
 
+// ✅ TEST ROUTE
 app.get("/", (req, res) => {
   res.send("AI Study Planner API Running");
 });
 
 
+// ✅ PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,12 @@
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-study-planner-backend-3nby.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
